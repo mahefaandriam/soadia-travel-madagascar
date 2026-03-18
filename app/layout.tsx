82254f6cb5121da1fe4e67cf8806f98d218f9704
mahefaandriam <92215850+mahefaandriam@users.noreply.github.com>
2026-03-18 11:52:50 +0300

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
+// app/layout.tsx
 import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ToastProvider } from "@/hooks/use-toast"
 import { SessionProvider } from "@/components/session-provider"
-import { Pacifico } from 'next/font/google';
+import { Pacifico } from 'next/font/google'
+import Script from "next/script"
 
 export const metadata: Metadata = {
   title: "Soa Dia Travel Madagascar",
   description: "Created by Connect Talent Dev",
   generator: "",
+  icons: "/favicon.png",
 }
 
 const pacifico = Pacifico({
@@ -17,7 +20,7 @@ const pacifico = Pacifico({
   weight: '400',
   variable: '--font-pacifico',
   display: 'swap', 
-});
+})
 
 export default function RootLayout({
   children,
@@ -26,6 +29,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" suppressHydrationWarning className={pacifico.variable}>
+      <head>
+        {/* Example: Global XM / Google Ads script */}
+        <Script
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8528915813394509"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <SessionProvider>
